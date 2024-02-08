fe7b486155845be8f0602e69df526ed25756098d
Thalles Oliveira <thalles3br@gmail.com>
2024-02-08 12:11:11 -0300

    Redis & Pub/Sub configuration

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,8 @@ import { FastifyInstance } from 'fastify'
 import { UUID, randomUUID } from 'node:crypto'
 import { z } from "zod"
 import { prisma } from "../../lib/prisma"
+import { redis } from '../../lib/redis'
+import { voting } from '../../utils/voting-pub-sub'
 
 
 export async function voteOnPoll(app: FastifyInstance) {
@@ -40,6 +42,14 @@ export async function voteOnPoll(app: FastifyInstance) {
           id: userVotedOnPoll.id
         }
       })
+
+      const votes = await redis.zincrby(pollId, -1, userVotedOnPoll.pollOptionId)
+
+      voting.publish(pollId, {
+        pollOptionId: userVotedOnPoll.pollOptionId,
+        votes: Number(votes)
+      })
+
     } else if (userVotedOnPoll) {
       return reply.status(400).send({message: 'You already voted on this poll'})
     }
@@ -61,6 +71,13 @@ export async function voteOnPoll(app: FastifyInstance) {
         pollId,
         pollOptionId,
       }
+    })
+
+    const votes = await redis.zincrby(pollId, 1, pollOptionId)
+
+    voting.publish(pollId, {
+      pollOptionId,
+      votes: Number(votes)
     })
 
     return reply.status(201).send()
